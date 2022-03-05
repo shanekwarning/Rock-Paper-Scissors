d0f30a174ca4efce51da53b77d56884d3ab387e1
Shane Warning <shane.k.warning@gmail.com>
2022-03-05 11:32:21 -0600

    Edit returns for function conditional

--- game.js
+++ game.js
@@ -4,19 +4,26 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1,
     this.player2 = player2
-    // this.gameData = gameData
   }
   checkWinner(player, computer) {
     if (player === 'rock' && computer === 'paper') {
-      return this.player2.wins += 1
+      this.player2.wins += 1;
+      return 'Computer Wins.'
     } else if (player === 'paper' && computer === 'scissors') {
-      return this.player2.wins += 1
+       this.player2.wins += 1;
+       return 'Computer Wins.'
     } else if (player === 'scissors' && computer === 'rock') {
-      return this.player2.wins += 1
+      this.player2.wins += 1;
+      return 'Computer Wins.'
     } else if (player === computer){
-        console.log('It\s a tie.')
+        return 'It\s a tie.'
       } else {
-        return this.player1.wins += 1
+        this.player1.wins += 1;
+        return 'You Win!'
       }
     }
 }
+
+var startGame = new Game(newPlayer, computerPlayer)
+
+startGame.checkWinner(newPlayer.name, computerPlayer.name)
