2fc98141507676143d90dbc4c747f56fbb855256
Shane Warning <shane.k.warning@gmail.com>
2022-03-05 11:33:03 -0600

    Move instances to class files

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var weaponsArray = ['Rock', 'Paper', 'Scissors']
 
-var newPlayer = new Player('rock')
-var computerPlayer = new Player('rock')
-var startGame = new Game(newPlayer, computerPlayer)
-
-
-
 var randomWeapon = weaponsArray[getRandomIndex(weaponsArray)]
 
 function getRandomIndex(weapon) {
     return  Math.floor(Math.random() * weapon.length)}
-
-startGame.checkWinner(newPlayer.name, computerPlayer.name)
