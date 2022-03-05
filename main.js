0f8ff6ae801190c12b708fe4a8d075c658e39671
Shane Warning <shane.k.warning@gmail.com>
2022-03-05 00:21:52 -0600

    Create test variables to see if game class conditionals work

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var weaponsArray = ['Rock', 'Paper', 'Scissors']
 
-var newPlayer = new Player
-var computerPlayer = new Player
+var newPlayer = new Player('rock')
+var computerPlayer = new Player('rock')
+var startGame = new Game(newPlayer, computerPlayer)
+
+
 
 var randomWeapon = weaponsArray[getRandomIndex(weaponsArray)]
 
 function getRandomIndex(weapon) {
     return  Math.floor(Math.random() * weapon.length)}
+
+startGame.checkWinner(newPlayer.name, computerPlayer.name)
