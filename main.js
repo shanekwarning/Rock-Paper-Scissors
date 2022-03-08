be276ff98341dde1f1c882fc15c77aad7c453fa1
Shane Warning <shane.k.warning@gmail.com>
2022-03-07 19:46:24 -0600

    Start to have weapon display function

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-var clickToPlayEasy = document.querySelector('.easy')
-var clickToPlayHard = document.querySelector('.hard')
-var clickToPlay = document.querySelectorAll('.difficulty-box')
-var weaponButtons = document.querySelectorAll('.weapon')
-var classicWeapons = document.querySelectorAll('.classic')
-var rules = document.querySelectorAll('.rules')
-var changeGameBtn = document.querySelector('.change-game')
-var chooseWeapon = document.querySelector('.weapon-icons')
-var gameBox = document.querySelector('.gamebox')
-var banner = document.querySelector('h2')
-var playerScore = document.querySelector('.player-score')
-var computerScore = document.querySelector('.computer-score')
-var weaponsArray = ['rock', 'paper', 'scissors', 'lizard', 'alien']
-
-var randomWeapon = weaponsArray[getRandomIndex(weaponsArray)]
+var clickToPlayEasy = document.querySelector(".easy");
+var clickToPlayHard = document.querySelector(".hard");
+var clickToPlay = document.querySelectorAll(".difficulty-box");
+var weaponButtons = document.querySelectorAll(".weapon");
+var classicWeapons = document.querySelectorAll(".classic");
+var rules = document.querySelectorAll(".rules");
+var changeGameBtn = document.querySelector(".change-game");
+var chooseWeapon = document.querySelector(".weapon-icons");
+var gameBox = document.querySelector(".gamebox");
+var banner = document.querySelector("h2");
+var playerScore = document.querySelector(".player-score");
+var computerScore = document.querySelector(".computer-score");
+var weaponsArray = ["rock", "paper", "scissors", "lizard", "alien"];
+var humanChoice = document.querySelector(".human-choice");
+var weaponChoice = document.querySelector(".weapon-choice");
+var randomWeapon = weaponsArray[getRandomIndex(weaponsArray)];
 
 var game;
 
@@ -32,101 +33,108 @@ var game;
 //   }
 //   }
 // }
-window.addEventListener('load', game)
+window.addEventListener("load", game);
 
+clickToPlayEasy.addEventListener("click", function () {
+  game.gameType = "classic";
+  // showWeapons(classicWeapons)
+  toggle(clickToPlay[0]);
+  toggle(clickToPlay[1]);
+  toggle(chooseWeapon);
+});
 
-clickToPlayEasy.addEventListener('click', function() {
-  game.gameType = 'classic'
-  showWeapons(classicWeapons)
-  toggle(clickToPlay[0])
-  toggle(clickToPlay[1])
-  toggle(chooseWeapon)
-})
+clickToPlayHard.addEventListener("click", function () {
+  game.gameType = "advanced";
+  showWeapons(weaponButtons);
+  toggle(clickToPlay[0]);
+  toggle(clickToPlay[1]);
+  toggle(chooseWeapon);
+});
 
-clickToPlayHard.addEventListener('click', function() {
-  game.gameType = 'advanced'
-  showWeapons(weaponButtons)
-  toggle(clickToPlay[0])
-  toggle(clickToPlay[1])
-  toggle(chooseWeapon)
-})
-
-changeGameBtn.addEventListener('click', function() {
+changeGameBtn.addEventListener("click", function () {
   for (var i = 0; i < weaponButtons.length; i++) {
-    hide(weaponButtons[i])
-    toggle(chooseWeapon)
-    show(clickToPlay[i])
+    hide(weaponButtons[i]);
+    toggle(chooseWeapon);
+    show(clickToPlay[i]);
   }
+});
 
-})
-
-chooseWeapon.addEventListener('click', function() {
-  playerWeaponChoice(event)
-  game.player2.takeTurn()
-  game.checkWinner(game.player1.currentWeapon, game.player2.currentWeapon)
-  game.displayScore()
-  compareWeapons()
-  setTimeout(roundTwo, 3000)
-})
+chooseWeapon.addEventListener("click", function () {
+  playerWeaponChoice(event);
+  game.player2.takeTurn();
+  game.checkWinner(game.player1.currentWeapon, game.player2.currentWeapon);
+  game.displayScore();
+  // compareWeapons()
+  setTimeout(roundTwo, 3000);
+});
 
 function playerWeaponChoice(event) {
-  mouseClick = event.target.className
-  selection = event.target.id
+  mouseClick = event.target.className;
+  selection = event.target.id;
   for (var i = 0; i < weaponButtons.length; i++) {
     if (weaponButtons[i].className === event.target.className) {
-    game.player1.currentWeapon = selection
+      game.player1.currentWeapon = selection;
     }
   }
 }
 function classic() {
   for (var i = 0; i < clickToPlay.length; i++) {
-    toggle(clickToPlay[i])
-    toggle(weaponButtons[i])
+    toggle(clickToPlay[i]);
+    toggle(weaponButtons[i]);
   }
 }
 
 function game() {
-  var newPlayer = new Player
-  var computerPlayer = new Player
-  game = new Game(newPlayer, computerPlayer)
+  var newPlayer = new Player();
+  var computerPlayer = new Player();
+  game = new Game(newPlayer, computerPlayer);
 }
 
 function getRandomIndex(num) {
-  return Math.floor(Math.random() * num)
+  return Math.floor(Math.random() * num);
 }
 
 function toggle(element) {
-  element.classList.toggle('hidden')
+  element.classList.toggle("hidden");
 }
 
 function hide(element) {
-  element.classList.add('hidden')
+  element.classList.add("hidden");
 }
 
 function show(element) {
-  element.classList.remove('hidden')
+  element.classList.remove("hidden");
 }
 
 function showWeapons(weapons) {
   for (var i = 0; i < weapons.length; i++) {
-    toggle(weapons[i])
+    toggle(weapons[i]);
   }
 }
 
+// function compareWeapons() {
+//   for (var i = 0; i < weaponButtons.length; i++) {
+//     if (
+//       weaponButtons[i].id !== game.player1.currentWeapon &&
+//       weaponButtons[i].id !== game.player2.currentWeapon
+//     ) {
+//       hide(weaponButtons[i]);
+//     }
+//   }
+// }
 
 function compareWeapons() {
-  for (var i = 0; i < weaponButtons.length; i++) {
-    if (weaponButtons[i].id !== game.player1.currentWeapon && weaponButtons[i].id !== game.player2.currentWeapon) {
-      hide(weaponButtons[i])
-    }
-  }
+  hide(chooseWeapon);
+  weaponChoice.classList.remove("hidden");
+  humanChoice.innerText = `game.player1.currentWeapon`;
+  computerChoice.innerText = `game.player2.currentWeapon`;
 }
 
 function roundTwo() {
   for (var i = 0; i < classicWeapons.length; i++) {
-    if (classicWeapons[i].classList.contains('hidden')) {
-      show(classicWeapons[i])
-      banner.innerText = 'Choose Your Weapon!'
+    if (classicWeapons[i].classList.contains("hidden")) {
+      show(classicWeapons[i]);
+      banner.innerText = "Choose Your Weapon!";
     }
   }
 }
